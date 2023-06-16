2ab2c85ba7984dc7be922058fee67f1160bcb5cc
ezrazka <ezra.a.einnos@gmail.com>
2023-06-16 19:16:17 +0700

    Removed unused comments.

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 "use strict"
 
-// function isElementInViewport(element) {
-//   let rect = element.getBoundingClientRect();
-
-//   return (
-//     rect.top >= 0 &&
-//     rect.left >= 0 &&
-//     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//   );
-// }
-
 window.addEventListener('scroll', () => {
   let scrollPosition = window.scrollY;
 
@@ -18,11 +7,4 @@ window.addEventListener('scroll', () => {
   document.querySelector('.main-heading-container').style.transform = `translateY(${scrollPosition * -0.3}px)`;
 
   document.querySelector('.main-nav-container').classList.toggle('background', scrollPosition > 0);
-
-  // let sections = document.querySelectorAll('section');
-  // let navItems = document.querySelectorAll('.main-nav ul li');
-
-  // sections.forEach((section, index) => {
-  //   navItems[index].classList.toggle('active', isElementInViewport(section));
-  // });
 });
